fe4de9e7ff8e8bd0197613dfeaef21bf56196212
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-02-02 21:39:35 +0300

    Задача №1. Показать двумерный массив размером mxn заполненный вещественными числами. Задача выполнена.

---
+++ Ex_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача №1. Показать двумерный массив размером m×n заполненный вещественными числами.
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} | ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matr)
+{   
+    Random random = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = Math.Round(random.NextDouble(), 2);            
+        }
+    }
+}
+
+Console.Write("Введите количество строк двумерного массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов двумерного массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+double[,] matrix = new double[rows, columns];
+
+FillArray(matrix);
+PrintArray(matrix);
